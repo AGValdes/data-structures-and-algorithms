f6b90932c2d14d89309ddb917a21a90a105232c8
ameilia valdes <valdes.ameilia@gmail.com>
2021-01-26 21:08:38 -0800

    have 2 tests for each method

--- dotnet/CodeChallenges/CodeChallengeTests/FifoAnimalShelterTests.cs
+++ dotnet/CodeChallenges/CodeChallengeTests/FifoAnimalShelterTests.cs
@@ -20,8 +20,20 @@ namespace CodeChallengeTests
     }
 
     [Fact]
+    public void Test_Multiple_Animals_Can_Be_Enqueued()
+    {
+      AnimalShelter testShelter = new AnimalShelter();
+      Cat testCat1 = new Cat();
+      Cat testCat2 = new Cat();
+      testShelter.Enqueue(testCat1);
+      testShelter.Enqueue(testCat2);
 
-    public void Test_Prefered_Animal_Is_Dequeued()
+      Assert.Equal(testCat2, testShelter.Rear);
+    }
+
+    [Fact]
+
+    public void Test_Prefered_Animal_Is_Dequeued_When_Not_Front()
     {
       AnimalShelter testShelter = new AnimalShelter();
       Cat testCat = new Cat();
@@ -31,6 +43,20 @@ namespace CodeChallengeTests
       Animal result = testShelter.Dequeue("dog");
 
       Assert.Equal(testDog, result);
+    }
+
+    [Fact]
+
+    public void Test_Returns_Null_If_Cat_Or_Dog_Isnt_Entered()
+    {
+      AnimalShelter testShelter = new AnimalShelter();
+      Cat testCat = new Cat();
+      Dog testDog = new Dog();
+      testShelter.Enqueue(testCat);
+      testShelter.Enqueue(testDog);
+      Animal result = testShelter.Dequeue("frog");
+
+      Assert.Null(result);
     }
   }
 }
