ef1eeb530262704ca73b1d489b34677977ec2f46
ameilia valdes <valdes.ameilia@gmail.com>
2021-02-01 14:30:44 -0800

    added trees implementations and tests

---
+++ dotnet/DataStructures/DataStructuresTests/Trees_Tests.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Implementations.Trees;
+using Xunit;
+
+namespace DataStructuresTests
+{
+  public class Trees_Tests
+  {
+    [Fact]
+    public void Test_For_Instanciating_Empty_Tree()
+    {
+      BinaryTree<int> testTree = new BinaryTree<int>();
+
+      Assert.Null(testTree.Root);
+    }
+
+    [Fact]
+
+    public void Test_For_Instanciating_Tree_With_One_Node()
+    {
+      BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+
+      testTree.Add(testTree.Root, 1);
+
+      Assert.Equal(1, testTree.Root.Value);
+    }
+
+    [Fact]
+    public void Test_For_Adding_Single_Left_and_Right_Node()
+    {
+      BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+
+      testTree.Add(testTree.Root, 10);
+      testTree.Add(testTree.Root, 8);
+      testTree.Add(testTree.Root, 12);
+      Assert.Equal(8, testTree.Root.Left.Value);
+      Assert.Equal(12, testTree.Root.Right.Value);
+    }
+
+    [Fact]
+    public void Test_For_Correct_Order_Using_PreOrder_Traversal()
+    {
+      List<int> input = new List<int>();
+      List<int> expected = new List<int> { 10, 8, 12 };
+      BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+
+      testTree.Add(testTree.Root, 10);
+      testTree.Add(testTree.Root, 8);
+      testTree.Add(testTree.Root, 12);
+      Assert.Equal(expected, testTree.PreOrder(testTree.Root, input));
+
+    }
+    [Fact]
+    public void Test_For_Correct_Order_Using_InOrder_Traversal()
+    {
+      List<int> input = new List<int>();
+      List<int> expected = new List<int> { 8, 10, 12 };
+      BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+
+      testTree.Add(testTree.Root, 10);
+      testTree.Add(testTree.Root, 8);
+      testTree.Add(testTree.Root, 12);
+      Assert.Equal(expected, testTree.InOrder(testTree.Root, input));
+    }
+    [Fact]
+    public void Test_For_Correct_Order_Using_PostOrder_Traversal()
+    {
+      List<int> input = new List<int>();
+      List<int> expected = new List<int> { 8, 12, 10 };
+      BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+
+      testTree.Add(testTree.Root, 10);
+      testTree.Add(testTree.Root, 8);
+      testTree.Add(testTree.Root, 12);
+      Assert.Equal(expected, testTree.PostOrder(testTree.Root, input));
+    }
+
+  }
+}
