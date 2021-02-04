81988022ee7b870ad19ee4b5c046d773ab5fd1a3
ameilia valdes <valdes.ameilia@gmail.com>
2021-02-03 21:51:31 -0800

    set up classes and updated readme

---
+++ dotnet/FizzBuzzTree/K-aryTree.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+  public class K_aryTree
+  {
+    public K_aryNode Root { get; set; }
+
+  public K_aryTree(K_aryNode root)
+    {
+      Root = root;
+    }
+  }
+
+}
