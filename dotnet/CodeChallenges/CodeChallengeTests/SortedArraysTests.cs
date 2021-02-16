a1e2909322e87b34569e60db413765e9afd30128
ameilia valdes <valdes.ameilia@gmail.com>
2021-02-15 20:13:37 -0800

    added logic for InsertionSort and unit tests. Updated readme

---
+++ dotnet/CodeChallenges/CodeChallengeTests/SortedArraysTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SortedArrays;
+
+namespace CodeChallengeTests
+{
+  public class SortedArraysTests
+  {
+
+  [Fact]
+   public void Test_That_InsertionSort_Outputs_Example_Array_In_Correct_Order()
+    {
+      int[] input = new int[] { 8, 4, 23, 42, 16, 15 };
+      int[] expected = new int[] { 4, 8, 15, 16, 23, 42 };
+      int[] result = Program.InsertionSort(input);
+
+      Assert.Equal(expected, result); 
+    }
+    [Fact]
+    public void Test_That_InsertionSort_Outputs_Any_Array_In_Correct_Order()
+    {
+      int[] input = new int[] { 6, 4, 2, 3, 5, 1 };
+      int[] expected = new int[] { 1, 2, 3, 4, 5, 6 };
+      int[] result = Program.InsertionSort(input);
+
+      Assert.Equal(expected, result);
+    }
+  }
+}
---
+++ dotnet/SortedArrays/README.md
@@ -0,0 +1,13 @@
+# Code Challenge 26: Insertion Sort
+
+## Challenge
+Create a blog article that shows the step-by-step algorithm for an Insertion Sort
+
+## Approach and Efficiency
+![Image of Step-By-Step Algorithm]()
+
+## API
+Public Methods: InsertionSort()
+
+
+
