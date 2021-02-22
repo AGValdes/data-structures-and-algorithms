c3cafa2a64f08e23c3b38e3841285a16e272d271
ameilia valdes <valdes.ameilia@gmail.com>
2021-02-21 21:54:10 -0800

    changed partition and swap

--- dotnet/SortedArrays/Program.cs
+++ dotnet/SortedArrays/Program.cs
@@ -41,21 +41,22 @@ namespace SortedArrays
       int low = left - 1;
       for (int i = left; i <= right; i++ )
       {
-        if(arr[i] <= pivot)
+        if(arr[i] < pivot)
         {
           low++;
           Swap(arr, i, low);
         }
       }
-      Swap(arr, right, low + 1);
+      arr = Swap(arr, right, low + 1);
       return low + 1;
     }
-    public static void Swap(int [] arr, int i, int low)
+    public static int[] Swap(int [] arr, int i, int low)
     {
       int temp;
       temp = arr[i];
       arr[i] = arr[low];
       arr[low] = temp;
+      return arr;
     }
  
   }
